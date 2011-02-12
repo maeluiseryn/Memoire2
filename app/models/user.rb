d2a42e1793402aa6b32199572045c46359633672
maeluiseryn <yodajesuis@hotmail.com>
2011-02-12 23:34:00 +0100

    draft user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  attr_accessible :name , :email
+  validates :name, :presence => true ,
+                   :length   => { :maximum => 50 },
+                   :uniqueness => true
+
+  validates :email, :presence => true ,
+                    :format => {:with => /(\A[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}\z)/i} ,
+                    :uniqueness => true
+
+  # email_regex =  /(\A[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}\z)/i
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110212201833) do
+ActiveRecord::Schema.define(:version => 20110212223014) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,5 +18,8 @@ ActiveRecord::Schema.define(:version => 20110212201833) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["name"], :name => "index_users_on_name", :unique => true
 
 end
